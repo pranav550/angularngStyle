4851635af51f81099968a03e6f74521931b7e0bb
pranav <pranavverma550@gmail.com>
2019-04-16 07:08:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getClosureSafeProperty } from '@angular/core/src/util/property';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularngStyle';
+
+  people:any[]=[
+{
+  "name":"aaaa",
+"country":"india"
+ },
+ {
+  "name":"bbbb",
+"country":"india"
+ },
+ {
+  "name":"cccc",
+"country":"uk"
+ },
+ {
+  "name":"dddd",
+"country":"uk"
+ },
+ {
+  "name":"eeee",
+"country":"usa"
+ },
+ {
+  "name":"ffff",
+"country":"usa"
+ },
+
+  ]
+
+  getcolor(country){
+      switch(country)
+      {
+        case 'india': return 'green';
+        case 'uk': return 'blue';
+        case 'usa': return 'red';
+      }
+  }
 }
